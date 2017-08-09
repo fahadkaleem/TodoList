128826a8e7c9e26d0f5834ef8052e13ee3e767d9
Mohammed Fahad Kaleem <mohammedfahadkaleem@gmail.com>
2017-08-09 17:22:17 -0500

    v2 requirements: Committed requirement 1

---
+++ v2.js
@@ -0,0 +1,10 @@
+/**
+ * Created by kaleemmf on 8/9/17.
+ */
+// v2 requirements
+//requirement 1
+var todos = ['item 1','item 2','item 3'];
+
+function displayTodos(){
+    console.log("My todos: ",todos);
+}
