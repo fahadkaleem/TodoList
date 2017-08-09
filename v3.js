34687d88d34c6a5e795c4872bcc0120307d9048f
Mohammed Fahad Kaleem <mohammedfahadkaleem@gmail.com>
2017-08-09 18:20:11 -0500

    v3 requirements Committed requirement 5

--- v3.js
+++ v3.js
@@ -2,21 +2,26 @@
  * Created by kaleemmf on 8/9/17.
  */
 
-//requirement 1
+// requirement 1
 var todos = {
     todos: ['item 1','item 2','item 3'],
     // requirement 2
     displayTodos: function(){
         console.log('My Todos', this.todos);
     },
-    //requirement 3
+    // requirement 3
     addTodos: function (newTodo) {
         this.todos.push(newTodo);
         this.displayTodos();
     },
-    //requirement 4
+    // requirement 4
     changeTodo: function(position,newValue){
         this.todos[position] = newValue;
+        this.displayTodos();
+    },
+    // requirement 5
+    deleteTodo: function(position){
+        this.todos.splice(position,1);
         this.displayTodos();
     }
 };
