d13ee6cf06a9b655d98e8b0064cb354634dbdcc6
Mohammed Fahad Kaleem <mohammedfahadkaleem@gmail.com>
2017-08-09 18:11:55 -0500

    v3 requirements Committed requirement 2

--- v3.js
+++ v3.js
@@ -4,5 +4,9 @@
 
 //requirement 1
 var todos = {
-    todos: ['item 1','item 2','item 3']
+    todos: ['item 1','item 2','item 3'],
+    // requirement 2
+    displayTodos: function(){
+        console.log('My Todos', this.todos);
+    }
 };
