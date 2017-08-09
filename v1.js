084bc5b2609c1aa6adc5b6ede9ceda8e234270f3
Mohammed Fahad Kaleem <mohammedfahadkaleem@gmail.com>
2017-08-09 16:58:00 -0500

    checked in requirement 5 for v1

--- v1.js
+++ v1.js
@@ -1,6 +1,7 @@
 /**
  * Created by kaleemmf on 8/9/17.
  */
+//v1 requirements
 
 //requirement 1
 var todos = ['item 1', 'item 2', 'item 3'];
@@ -11,5 +12,8 @@ console.log("My todos: ", todos);
 //requirement 3
 todos.push("item 4");
 
-// requirements 4
+// requirement 4
 todos[0] = "new item 1";
+
+// requirement 5
+todos.splice(0,1);
